28d89ec9ee1e65ddf49b963d2befa595aa8ab200
yearbook-milk <throwaway0x069420@gmail.com>
2023-08-02 10:07:29 -0400

    minor sperlling mistake detecdteed

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ pin_config = {                                      # These control which pins o
 "yawPin": 12,
 "afterSpdCmdDelay": 0,                              # This should be set to 0, in order to make the pitch and yaw cmds nonblocking.
 "pulse_freq": 50,                                    # Pulse frequency, in hZ.
-"pinsToSet": "leftPin rightPin yawPin"              # DONT CHANGE THIS 
+"pinsToSet": "leftPin rightPin yawPin",             # DONT CHANGE THIS 
 
 
 "revPin": 23,
